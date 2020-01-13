3f479456ffe7b6e5cf0559cacc3529f23fa9db48
Sean Shaifubahrim <sean.s1112@gmail.com>
2020-01-13 11:43:25 -0700

    aws-signedurl example

---
+++ nodejs-aws-s3signature/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const aws = require('aws-sdk');
+
+//Config
+const app = express();
+dotenv.config();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Get signed url
+app.get('/get-signed-url', async (req, res) => {
+    aws.config.update({
+        accessKeyId: process.env.S3_ID,
+        secretAccessKey: process.env.S3_KEY,
+        region: process.env.S3_REGION,
+        signatureVersion: 'v4'
+    });
+
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: 'test/upload_test.mp4',
+        Expires: 30 * 60, //30 min
+        ContentType: 'video/mp4'
+    };
+
+    const options = {
+        signatureVersion: 'v4',
+        region: process.env.S3_REGION,
+        endpoint: new aws.Endpoint('elasticbeanstalk-us-west-2-857872516664.s3-accelerate.amazonaws.com'), 
+        useAccelerateEndpoint: true
+    };
+
+    const client = new aws.S3(options);
+
+    const signedURL = await (new Promise((resolve, reject) => {
+        client.getSignedUrl('putObject', params, (err, data) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        });
+    }));
+
+    return res.json({
+        signedURL
+    });
+});
+
+//Routes
+app.use('/', require('./routes/index'));
+
+
+//Port
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
+
---
+++ nodejs-aws-s3signature/public/javascripts/upload.js
@@ -0,0 +1,54 @@
+console.log('Hello World.');
+
+initiateUpload = () => {
+    const files = document.getElementById('input-file').files;
+    console.log(files);
+    const file = files[0];
+
+    if(file == null){
+        console.log('No file.');
+    }else{
+        getSignedURL(file);
+    }
+
+}
+
+getSignedURL = (file) => {
+    const xhr = new XMLHttpRequest();
+    let resp;
+
+    xhr.open('GET', '/get-signed-url', true);
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                console.log('Got signed URL...');
+                resp = JSON.parse(xhr.response);
+                console.log(resp.signedURL);
+                //console.log(resp.signedURL);
+                uploadFile(file, resp.signedURL);
+            }else{
+                console.log('Error not 200');
+            }
+        }
+    }
+
+    xhr.send();
+}
+
+uploadFile = (file, signed_url) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('PUT', signed_url);
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                console.log('Upload success.');
+                //console.log(xhr.response);
+            }else{
+                console.log('Upload failed.');
+            }
+        }
+    }
+
+    xhr.send(file);
+}
